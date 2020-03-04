bfe16243e4abd84f98725ad620ae3ce7504d9ce9
Bill Sanders <billysanders@gmail.com>
2020-03-04 22:02:40 +0000

    verbose++

--- sles/src/stack/images/common/images-sles.mk
+++ sles/src/stack/images/common/images-sles.mk
@@ -1,11 +1,14 @@
 RPMLOC = $(shell find cache -type f -name *.rpm)
-
+$(info RPMLOC is $(RPMLOC))
+$(info PATH is $(shell pwd))
+$(info contents is $(shell find))
 localrepo:
+	$(info making localrepo in $(CURDIR))
 	mkdir -p $(CURDIR)/localrepo
 	@echo $(CURDIR)/../../../../../RPMS
 	@echo $(REDHAT.ROOT)
 	ln -s $(CURDIR)/../../../../../RPMS $(CURDIR)/localrepo 
-	createrepo $(CURDIR)/localrepo
+	createrepo -v $(CURDIR)/localrepo
 	@echo -e "[localdir] \n\
 name=local \n\
 baseurl=file://$(CURDIR)/localrepo\n\
@@ -17,5 +20,6 @@ getpackages:
 	rm -rf cache
 	mkdir -p cache
 	zypper --pkg-cache-dir cache --reposd-dir $(CURDIR) clean --all
+	$(info PATH is $(shell pwd))
 	zypper --pkg-cache-dir cache --reposd-dir $(CURDIR) download $(YUMLIST)
 
