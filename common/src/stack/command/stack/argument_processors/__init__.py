34f6279c929088bdd10b5994f087a769d20c03ea
Bill Sanders <billysanders@gmail.com>
2020-03-26 22:04:46 -0700

    fixup! fixup! fixup! fixup! reorder and fix imports

--- common/src/stack/command/stack/argument_processors/__init__.py
+++ common/src/stack/command/stack/argument_processors/__init__.py
@@ -14,11 +14,11 @@ pkg_paths = {fi.parent for fi in path.glob('*.py') if fi.parent != '__init__.py'
 # get all arg_proc modules loaded...
 arg_proc_pkg = {}
 for _, name, _ in pkgutil.walk_packages(pkg_paths):
-        arg_proc_pkg[name] = importlib.import_module('.' + name, 'stack.argument_processors')
+	arg_proc_pkg[name] = importlib.import_module('.' + name, 'stack.argument_processors')
 
 # for each module, find any '*ArgProcessor' class add it to the stack.argument_processors namespace
 for module in arg_proc_pkg.values():
-        for _, cls in inspect.getmembers(module, inspect.isclass):
-                if cls.__name__.endswith('ArgProcessor'):
-                        globals()[cls.__name__] = cls
+	for _, cls in inspect.getmembers(module, inspect.isclass):
+		if cls.__name__.endswith('ArgProcessor'):
+			globals()[cls.__name__] = cls
 			__all__.append(cls.__name__)
